8ce923f2e3059c68355ea6507507b3d0ced70a2f
unknown <487885098@qq.com>
2017-12-12 21:55:27 +0800

    移动端静态网页

--- public/mobile_my/js/common.js
+++ public/mobile_my/js/common.js
@@ -1,3 +1,14 @@
-mui('.mui-scroll-wrapper').scroll({
-	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-});
+$(function () {
+	mui('.mui-scroll-wrapper').scroll({
+		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+	});
+	$('span.fa-search').on('tap',function () {  
+		window.location.href="./search.html";
+	});
+	$('span.fa-home').on('tap',function () {  
+		window.location.href="./index.html";
+	});
+	$('span.fa-chevron-left').on('tap',function () {  
+		window.history.back();
+	});
+})
